db4b13646213dfbbb1f13944375fd0fe5634b980
Akp9 <amitkumar_p9@outlook.com>
2017-03-07 09:42:33 +0000

    Add files via upload

---
+++ Developing scalable apps Udacity/src/main/java/com/google/devrel/training/conference/Constants.java
@@ -0,0 +1,17 @@
+package com.google.devrel.training.conference;
+
+import com.google.api.server.spi.Constant;
+
+/**
+ * Contains the client IDs and scopes for allowed clients consuming the conference API.
+ */
+public class Constants {
+    public static final String WEB_CLIENT_ID = "437417304285-lnb3unarsdh5nv8frcr848sk5omckqq3.apps.googleusercontent.com";
+    public static final String ANDROID_CLIENT_ID = "437417304285-3kh8qn6j8cr3690pg8ue3gm7uhk7ft5c.apps.googleusercontent.com";
+    public static final String IOS_CLIENT_ID = "replace this with your iOS client ID";
+    public static final String ANDROID_AUDIENCE = WEB_CLIENT_ID;
+    public static final String EMAIL_SCOPE = Constant.API_EMAIL_SCOPE;
+    public static final String API_EXPLORER_CLIENT_ID = Constant.API_EXPLORER_CLIENT_ID;
+
+    public static final String MEMCACHE_ANNOUNCEMENTS_KEY = "RECENT_ANNOUNCEMENTS";
+}
